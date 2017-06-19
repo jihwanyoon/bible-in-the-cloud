a4348fc0b5e425638c50bc657d6e825f5be11a4f
Jihwan Yoon <Ji@Jihwans-MBP.home>
2017-06-19 18:11:15 -0400

    Added script to upload Genesis to database

--- migration/import_verses.py
+++ migration/import_verses.py
@@ -9,7 +9,19 @@ if __name__ == '__main__':
 	# Set up the database
 	db = Database(HOST, USER, PASSWORD, DB)
 
-	# TODO: Load 001.txt and insert each line into the verses table	
+	# Load 001.txt and insert each line into the verses table	
+	book = open("bible_versions/krv/001.txt", "r")
 
+	bookid = db.find_one("SELECT id FROM books WHERE id = 1")[0]
+
+	for line in book:
+		chapter = line[:3]
+		verse = line[4:7]
+		text = line[8:]
+
+		db.execute_many("""
+			INSERT INTO verses (version_id, book_id, chapter, verse, text) 
+			VALUES (2, %s, %s, %s, '%s')""" % (bookid, chapter, verse, text))
+	
 	# Close the db connection
 	db.close()
