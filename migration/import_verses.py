b8d76319577938b6814030343eb6b21496d10eef
Jihwan Yoon <Ji@Jihwans-MBP.home>
2017-06-21 23:10:08 -0400

    code so far to import all verses

--- migration/import_verses.py
+++ migration/import_verses.py
@@ -1,4 +1,5 @@
 from database import Database
+import os
 
 HOST = 'localhost'
 USER = 'bitc'
@@ -10,22 +11,31 @@ if __name__ == '__main__':
 	db = Database(HOST, USER, PASSWORD, DB)
 
 	# Load 001.txt and insert each line into the verses table	
-	file = open("bible_versions/krv/001.txt", "r")
+	path = '/Users/Ji/dev/bible-in-the-cloud/migration/bible_versions/krv'
+	files = []
+	book_id = []
+	for filename in os.listdir(path):
+		file = open("bible_versions/krv/" + filename, "r")
+		book_id.append(filename[:3])
+		files.append(file)
 
-	book_id = db.find_one("SELECT id FROM books WHERE name = 'genesis'")['id']
 	version_id = db.find_one("SELECT id FROM versions WHERE code = 'krv'")['id']
 
 	args = []
-	for line in file:
-		chapter = int(line[:3])
-		verse = int(line[4:7])
-		text = line[8:]
+	index = 0
 
-		args.append((version_id, book_id, chapter, verse, text))
+	for file in files[1:]:
+		for line in file:
+			chapter = int(line[:3])
+			verse = int(line[4:7])
+			text = line[8:]
 
-	db.execute_many("""
-		INSERT INTO verses (version_id, book_id, chapter, verse, text) 
-		VALUES (%s, %s, %s, %s, %s)""", args)
+			args.append((version_id, book_id[index], chapter, verse, text))
+		index += 1
+
+		db.execute_many("""
+			INSERT INTO verses (version_id, book_id, chapter, verse, text) 
+			VALUES (%s, %s, %s, %s, %s)""", args)
 	
 	# Close the db connection
 	db.close()
