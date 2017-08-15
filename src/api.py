a405f9e1162d422ed7aef30659e3ccb7ce5cd6db
Jihwan Yoon <Ji@Jihwans-MBP.home>
2017-08-15 11:49:53 -0400

    Acces a webpage for any chapter of a book in KRV

--- src/api.py
+++ src/api.py
@@ -10,9 +10,5 @@ manager.create_api(DisplayName)
 manager.create_api(Verses)
 manager.create_api(Version)
 
-# SERIN i dont think our website would need post or delete right? unless we're using this to add new version like ESV etc.
-# if you want to post or delete, you need to specity
-# manager.create_api(Language, methods=['GET', 'POST', 'DELETE', 'PUT'])
-
 if __name__ == "__main__":
 	app.run(debug=True)
--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
+from models import *
 
 app = Flask(__name__)
 
@@ -11,9 +13,46 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://bitc:@localhost/bitc'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-@app.route('/')
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'bitc'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'bitc'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+# init MYSQL
+mysql = MySQL(app)
+
+
+# Index
+@app.route('/', methods=['POST', 'GET'])
 def index():
-	return 'hello world'
+	# Contains all Versions, Books, and Chapters
+	versions = Version.query.all()
+	books = Book.query.all()
+
+	# If a search for verses have been made
+	if request.method == 'POST':
+		version = request.form.get('version')
+		book = request.form.get('book')
+		chapter = request.form.get('chapter')
+		return redirect(url_for('view_chapter', version=version, book=book, chapter=chapter))
+
+	return render_template('home.html', versions=versions, books=books)
+
+# Load full verses of a chapter
+@app.route('/<version>/<book>/<chapter>', methods=['GET', 'POST']) 
+def view_chapter(version, book, chapter):
+
+
+	book_id = Book.query.filter_by(name=book).first().id
+	version_id = Version.query.filter_by(code=version).first().id
+	chapter = chapter
+	verses = Verses.query.filter_by(version_id=version_id, book_id=book_id, chapter=chapter)
+
+	return render_template('view_chapter.html', verses=verses, version_id=version_id, book_id=book_id, chapter=chapter, book=book, version=version)
+
 
 if __name__ == '__main__':
 	app.run()
+
